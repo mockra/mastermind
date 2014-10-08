c630e8008e98051268b547be1732cccefe99cdcc
David Ratajczak <david@mockra.com>
2014-10-08 15:54:06 -0700

    adds base mastermind script

---
+++ mastermind.rb
@@ -0,0 +1,8 @@
+require_relative 'lib/cli'
+require_relative 'lib/code_sequence'
+require_relative 'lib/game'
+require_relative 'lib/round'
+require_relative 'lib/sequence_comparison'
+require_relative 'lib/sequence_message'
+
+Cli.new.run
