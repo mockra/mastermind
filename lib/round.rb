2df567ad7f936b601f75377eb7b8ea4ad102dc3d
David Ratajczak <david@mockra.com>
2014-10-08 20:30:44 -0700

    updates play intro message

--- lib/round.rb
+++ lib/round.rb
@@ -7,7 +7,9 @@ class Round
   end
 
   def play
-    puts 'Enter your guess'
+    puts "I have generated a beginner sequence with four elements made up of:"\
+      "(r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end "\
+      "the game.\n What's your guess?"
     begin
       comp = comparison(gets.downcase.strip.split(''))
       self.num_of_guesses += 1
